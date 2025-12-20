aaa26f7d383e85796138dc7904e6f6cfc9215359
Addi3 <addiec787@gmail.com>
2025-12-20 17:39:33 +0000

    feat: soundprovider feat: differinghandmodelitem feat: readme feat: logos / promo

---
+++ src/main/java/com/lib/core/items/DifferingHandModelItem.java
@@ -0,0 +1,7 @@
+package com.lib.core.items;
+
+// This is for items that have different models depending on if it's in/on the inventory/ground and in the hand.
+
+public interface DifferingHandModelItem {
+
+}
